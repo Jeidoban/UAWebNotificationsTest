ec0a99a8ec23463cb668b62e04b2a5b247d6b9cd
Joafie <jade424433@gmail.com>
2018-07-19 14:40:29 -0700

    UA installed

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1532034407.0
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://joafie.github.io/UAWebNotificationsTest/images/UA.png',
+  defaultTitle: 'UAWebNotificationsTest',
+  defaultActionURL: 'https://joafie.github.io/UAWebNotificationsTest/',
+  appKey: '92tyYWuXSf6eCc8Urs4FqA',
+  token: 'MTo5MnR5WVd1WFNmNmVDYzhVcnM0RnFBOkRxYkFuaGc5MHpKVUZGU2tPUTljRUt3UzlPa1EzSGpEXzR4OUZqQlFsQkk',
+  vapidPublicKey: 'BLbGUlCertRZFg_ssufWt_7EEyNh0ua_3TeTjkGdZmb25qHeSQu4f-BXpSLC2NwGqhELWw14G6axE98412ZclJo='
+})
